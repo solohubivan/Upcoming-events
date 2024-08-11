d41491e41a9bcb26842d02e8316003460952bb12
solohubivan <solohubivan@gmail.com>
2024-08-11 16:26:07 +0300

    Initial Commit

---
+++ Upcoming events/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Upcoming events
+//
+//  Created by Ivan Solohub on 11.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
