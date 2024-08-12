fc7001afb7c57b72c260d509eac1a3b60156089d
solohubivan <solohubivan@gmail.com>
2024-08-12 13:58:46 +0300

    added colors

--- Upcoming events/Extensions/UIColor+Extension.swift
+++ Upcoming events/Extensions/UIColor+Extension.swift
@@ -8,5 +8,6 @@
 import UIKit
 
 extension UIColor {
-    static let hex231F20 = UIColor(red: 0.1373, green: 0.1216, blue: 0.1255, alpha: 1.0)
+    static let hex5856D6 = UIColor(red: 0.3451, green: 0.3373, blue: 0.8392, alpha: 1.0)
+    static let hex7676801F = UIColor(red: 0.4627, green: 0.4627, blue: 0.5020, alpha: 0.1216)
 }
